49900b18d1fe9b70d0e0c011ef298ab33bc2b122
Bobb0404 <bobmhando@gmail.com>
2025-08-08 10:52:49 +0300

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -1,42 +1,17 @@
 const puzzles = {
-    "DS0001B": {
-        size: 3,
-        clues: [
-            { row: 1, col: 1, letter: "B" },
-            { row: 1, col: 3, letter: "D" },
-            { row: 2, col: 3, letter: "D" },
-            { row: 3, col: 1, letter: "B" },
-            { row: 3, col: 3, letter: "E" }
-        ],
-        words: ["BAD", "END", "BYE", "DAD"]
-    },
-    "DS0001R": {
-        size: 5,
-        clues: [
-            { row: 1, col: 1, letter: "C" },
-            { row: 1, col: 5, letter: "P" },
-            { row: 2, col: 1, letter: "N" },
-            { row: 3, col: 1, letter: "E" },
-            { row: 3, col: 5, letter: "R" },
-            { row: 5, col: 1, letter: "C" },
-            { row: 5, col: 5, letter: "E" }
-        ],
-        words: ["CREEP", "NORMS", "ETHER", "CANOE", "EARTH", "POSER"]
-    },
-    "DS0001E": {
-        size: 7,
-        clues: [
-            { row: 1, col: 1, letter: "M" },
-            { row: 1, col: 7, letter: "E" },
-            { row: 2, col: 1, letter: "T" },
-            { row: 3, col: 1, letter: "H" },
-            { row: 3, col: 7, letter: "N" },
-            { row: 4, col: 1, letter: "R" },
-            { row: 4, col: 7, letter: "E" },
-            { row: 5, col: 1, letter: "M" },
-            { row: 7, col: 1, letter: "M" },
-            { row: 7, col: 7, letter: "E" }
-        ],
-        words: ["MISTAKE", "TAMPERS", "HALOGEN", "RESIDUE", "MATCHER", "SAMPLES", "AVENGED", "ESSENCE"]
-    }
+  DS0001B: {
+    size: 3,
+    across: ["BaD", "EnD"],
+    down: ["ByE", "DaD"]
+  },
+  DS0001R: {
+    size: 5,
+    across: ["CREEP", "NORMS", "ETHeR"],
+    down: ["CANOE", "EARTH", "POSER"]
+  },
+  DS0001E: {
+    size: 7,
+    across: ["MISTAKE", "TAMPERS", "HALOGEN", "RESIDUE"],
+    down: ["MATCHER", "SAMPLES", "AVENGED", "ESSENCE"]
+  }
 };
