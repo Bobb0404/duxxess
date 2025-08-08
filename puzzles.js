132e75f4534a00254f9e9dd605dd71ae527ef38e
Bobb0404 <bobmhando@gmail.com>
2025-08-08 10:23:40 +0300

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -1,13 +1,53 @@
+// puzzles.js
+// Duxxess Puzzles Data
+
 const puzzles = {
     "DS0001B": {
         size: 3,
-        across: [
-            { clue: "Not soft", answer: "HARD", row: 1, col: 1 },
-            { clue: "Opposite of stop", answer: "GO", row: 3, col: 1 }
+        clues: [
+            ["B", "", "D"],
+            ["", "#", ""],
+            ["E", "", "Y"]
         ],
-        down: [
-            { clue: "To own", answer: "HAD", row: 1, col: 1 },
-            { clue: "Exists", answer: "ARE", row: 1, col: 3 }
-        ]
+        words: ["BAD", "END", "BYE", "BED", "DAY", "BAY", "DEN", "BEY"]
+    },
+
+    "DS0002R": {
+        size: 5,
+        clues: [
+            ["E", "", "X", "", "E"],
+            ["", "#", "", "#", ""],
+            ["C", "", "E", "", "L"],
+            ["", "#", "", "#", ""],
+            ["L", "", "N", "", "T"]
+        ],
+        words: ["EXCEL", "LENT", "CELL", "CENT", "TEN", "NET", "LET", "LEX"]
+    },
+
+    "DS0003M": {
+        size: 7,
+        clues: [
+            ["S", "", "P", "", "A", "", "R"],
+            ["", "#", "", "#", "", "#", ""],
+            ["E", "", "A", "", "S", "", "T"],
+            ["", "#", "", "#", "", "#", ""],
+            ["C", "", "O", "", "A", "", "S"],
+            ["", "#", "", "#", "", "#", ""],
+            ["T", "", "S", "", "E", "", "A"]
+        ],
+        words: ["SPAR", "EAST", "COAST", "SEA", "STAR", "COST", "SAT", "ART"]
     }
 };
+
+// Default puzzle ID
+let currentPuzzleId = "DS0001B";
+
+// Load a puzzle by ID
+function loadPuzzle(puzzleId) {
+    if (puzzles[puzzleId]) {
+        currentPuzzleId = puzzleId;
+        renderPuzzle(puzzles[puzzleId]);
+    } else {
+        alert("Puzzle not found!");
+    }
+}
