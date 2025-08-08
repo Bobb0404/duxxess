e2f7bc8f0f61b41c341c6bfa0baf1d0e520e9f55
Bobb0404 <bobmhando@gmail.com>
2025-08-08 09:51:57 +0300

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -1,32 +1,38 @@
 // puzzles.js
-// Duxxess Square puzzle data
-
 const puzzles = {
     "DS0001B": {
         size: 3,
-        clues: {
-            "R1C1": "B", "R1C3": "A",
-            "R3C1": "D", "R3C3": "E"
-        },
-        words: ["BAD", "ACE", "BED", "DAB", "CAB", "ACE", "BED", "BAD"]
+        clues: [
+            ["B", "", "D"],
+            ["", "#", ""],
+            ["E", "", "D"]
+        ],
+        words: ["BAD", "END", "BYE", "BED", "DAD", "BEE", "DEN", "RED"]
     },
-    "DS0001R": {
+    "DS0002R": {
         size: 5,
-        clues: {
-            "R1C1": "H", "R1C3": "O", "R1C5": "P",
-            "R3C1": "E", "R3C5": "E",
-            "R5C1": "R", "R5C3": "O", "R5C5": "D"
-        },
-        words: ["HOPE", "RODE", "HERD", "PORE", "REDO", "DOER", "HOPED", "ROPE"]
+        clues: [
+            ["T", "", "", "", "E"],
+            ["", "#", "", "#", ""],
+            ["", "", "A", "", ""],
+            ["", "#", "", "#", ""],
+            ["S", "", "", "", "T"]
+        ],
+        words: ["TEASE", "START", "SEA", "SET", "EAST", "SAT", "ART", "TEST"]
     },
-    "DS0001E": {
+    "DS0003E": {
         size: 7,
-        clues: {
-            "R1C1": "M", "R1C3": "A", "R1C5": "S", "R1C7": "T",
-            "R3C1": "E", "R3C7": "R",
-            "R5C1": "P", "R5C7": "S",
-            "R7C1": "L", "R7C3": "A", "R7C5": "N", "R7C7": "E"
-        },
-        words: ["MASTER", "PLANES", "REACT", "STONE", "MAPLES", "CLEAN", "PLANTS", "SMARTEN"]
+        clues: [
+            ["C", "", "", "", "", "", "T"],
+            ["", "#", "", "#", "", "#", ""],
+            ["", "", "A", "", "R", "", ""],
+            ["", "#", "", "#", "", "#", ""],
+            ["", "", "E", "", "S", "", ""],
+            ["", "#", "", "#", "", "#", ""],
+            ["P", "", "", "", "", "", "S"]
+        ],
+        words: ["CARTERS", "CAPTESS", "CAT", "PET", "CAR", "ART", "REST", "STEP"]
     }
 };
+
+let defaultPuzzleId = "DS0001B";
