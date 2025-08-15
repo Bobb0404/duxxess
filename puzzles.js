58778dd5f1b2d7f02f537833f84d308fa970c107
Bobb0404 <bobmhando@gmail.com>
2025-08-15 23:59:31 +0300

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -1,14 +1,40 @@
+// MSINGI: Latest stable version with 3 brand new puzzles
+
 const puzzles = {
     "DS0001B": {
+        size: 3,
+        defaultLoad: true,
+        grid: [
+            ["B","A","D"],
+            ["Y"," ","N"],
+            ["D","A","D"]
+        ],
+        solutionWords: ["BAD","END","BYE","DAD","B","A","D","E"]
+    },
+
+    "DS0001R": {
+        size: 5,
+        grid: [
+            ["C","R","E","E","P"],
+            ["A"," ","R"," ","S"],
+            ["N","O","R","M","S"],
+            ["E"," ","E"," ","R"],
+            ["E","T","H","E","R"]
+        ],
+        solutionWords: ["CREEP","NORMS","ETHER","CANOE","EARTH","POSER","C","E","R"]
+    },
+
+    "DS0001E": {
         size: 7,
         grid: [
-            ["B","A","D","C","A","T","S"],
-            ["E"," ","N"," ","D"," "," "],
-            ["L","I","D","G","I","G"," "],
-            [" "," "," "," "," "," "," "],
-            ["C","R","E","E","P"," "," "],
-            [" "," "," "," "," "," "," "],
-            ["G","O","A","T","S"," "," "]
-        ]
+            ["M","I","S","T","A","K","E"],
+            ["A"," ","T"," ","P"," ","S"],
+            ["T","M","P","E","R","S"," "],
+            ["C"," ","H"," ","L"," ","G"],
+            ["H","A","L","O","G","E","N"],
+            ["E"," ","R"," ","E"," ","E"],
+            ["R","E","S","I","D","U","E"]
+        ],
+        solutionWords: ["MISTAKE","TAMPERS","HALOGEN","RESIDUE","MATCHER","SAMPLES","AVENGED","ESSENCE"]
     }
 };
